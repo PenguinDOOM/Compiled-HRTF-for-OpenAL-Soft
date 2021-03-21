aabd135eaa194efe0b1f75dd8a19e94ed5b05e99
Penguin <hibi414sa@gmail.com>
2021-03-22 01:18:23 +0900

    The binary is 32-bit, so it needs to be done in mono.

--- SADIE.bat
+++ SADIE.bat
@@ -1,13 +1,13 @@
 cd SADIE
 
-..\makemhr -r 44100 -j 2 -d dataset -i SADIE_KEMAR.def -o ..\artifact\44100\sadie_kemar_44100_dataset.mhr
-..\makemhr -r 44100 -j 2 -d sphere -i SADIE_KEMAR.def -o ..\artifact\44100\sadie_kemar_44100_sphere.mhr
-..\makemhr -r 48000 -j 2 -d dataset -i SADIE_KEMAR.def -o ..\artifact\48000\sadie_kemar_48000_dataset.mhr
-..\makemhr -r 48000 -j 2 -d sphere -i SADIE_KEMAR.def -o ..\artifact\48000\sadie_kemar_48000_sphere.mhr
+..\makemhr -r 44100 -j 2 -d dataset -i SADIE_KEMAR_mono.def -o ..\artifact\44100\sadie_kemar_mono_44100_dataset.mhr
+..\makemhr -r 44100 -j 2 -d sphere -i SADIE_KEMAR_mono.def -o ..\artifact\44100\sadie_kemar_mono_44100_sphere.mhr
+..\makemhr -r 48000 -j 2 -d dataset -i SADIE_KEMAR_mono.def -o ..\artifact\48000\sadie_kemar_mono_48000_dataset.mhr
+..\makemhr -r 48000 -j 2 -d sphere -i SADIE_KEMAR_mono.def -o ..\artifact\48000\sadie_kemar_mono_48000_sphere.mhr
 
-..\makemhr -r 44100 -j 2 -d dataset -i SADIE_KU100.def -o ..\artifact\44100\sadie_ku100_44100_dataset.mhr
-..\makemhr -r 44100 -j 2 -d sphere -i SADIE_KU100.def -o ..\artifact\44100\sadie_ku100_44100_sphere.mhr
-..\makemhr -r 48000 -j 2 -d dataset -i SADIE_KU100.def -o ..\artifact\48000\sadie_ku100_48000_dataset.mhr
-..\makemhr -r 48000 -j 2 -d sphere -i SADIE_KU100.def -o ..\artifact\48000\sadie_ku100_48000_sphere.mhr
+..\makemhr -r 44100 -j 2 -d dataset -i SADIE_KU100_mono.def -o ..\artifact\44100\sadie_ku100_mono_44100_dataset.mhr
+..\makemhr -r 44100 -j 2 -d sphere -i SADIE_KU100_mono.def -o ..\artifact\44100\sadie_ku100_mono_44100_sphere.mhr
+..\makemhr -r 48000 -j 2 -d dataset -i SADIE_KU100_mono.def -o ..\artifact\48000\sadie_ku100_mono_48000_dataset.mhr
+..\makemhr -r 48000 -j 2 -d sphere -i SADIE_KU100_mono.def -o ..\artifact\48000\sadie_ku100_mono_48000_sphere.mhr
 
 cd ..\
