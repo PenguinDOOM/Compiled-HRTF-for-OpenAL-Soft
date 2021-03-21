f805bd2b50d5e3fb74f40f0bab7c46343ff29d2d
Penguin <hibi414sa@gmail.com>
2021-03-22 01:03:23 +0900

    Add the necessary files.

---
+++ ClubFritz.bat
@@ -0,0 +1,13 @@
+cd ClubFritz
+
+..\makemhr -r 44100 -j 2 -d dataset -i ClubFritz_KU100.def -o ..\artifact\44100\clubfritz_ku100_mono_44100_dataset.mhr
+..\makemhr -r 44100 -j 2 -d sphere -i ClubFritz_KU100.def -o ..\artifact\44100\clubfritz_ku100_mono_44100_sphere.mhr
+..\makemhr -r 48000 -j 2 -d dataset -i ClubFritz_KU100.def -o ..\artifact\48000\clubfritz_ku100_mono_48000_dataset.mhr
+..\makemhr -r 48000 -j 2 -d sphere -i ClubFritz_KU100.def -o ..\artifact\48000\clubfritz_ku100_mono_48000_sphere.mhr
+
+..\makemhr -r 44100 -j 2 -d dataset -i ClubFritz_KU100_Stereo.def -o ..\artifact\44100\clubfritz_ku100_stereo_44100_dataset.mhr
+..\makemhr -r 44100 -j 2 -d sphere -i ClubFritz_KU100_Stereo.def -o ..\artifact\44100\clubfritz_ku100_stereo_44100_sphere.mhr
+..\makemhr -r 48000 -j 2 -d dataset -i ClubFritz_KU100_Stereo.def -o ..\artifact\48000\clubfritz_ku100_stereo_48000_dataset.mhr
+..\makemhr -r 48000 -j 2 -d sphere -i ClubFritz_KU100_Stereo.def -o ..\artifact\48000\clubfritz_ku100_stereo_48000_sphere.mhr
+
+cd ..\
