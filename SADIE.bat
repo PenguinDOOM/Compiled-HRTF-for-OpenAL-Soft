53aee62547fead642f6ba18e725dcb5e3043d05e
Penguin <hibi414sa@gmail.com>
2021-03-22 20:25:02 +0900

    Added a human model.
    Maybe... Inaccurate.

--- SADIE.bat
+++ SADIE.bat
@@ -10,4 +10,11 @@ cd SADIE
 ..\makemhr -r 48000 -j 2 -d dataset -i SADIE_KU100_mono.def -o ..\artifact\48000\sadie_ku100_mono_48000_dataset.mhr
 ..\makemhr -r 48000 -j 2 -d sphere -i SADIE_KU100_mono.def -o ..\artifact\48000\sadie_ku100_mono_48000_sphere.mhr
 
+for /R .\ %%f in (SADIE_Human*_mono.def) do (
+  ..\makemhr -r 44100 -j 2 -d dataset -i %%f -o ..\artifact\44100\%%~nf_44100_dataset.mhr
+  ..\makemhr -r 44100 -j 2 -d sphere -i %%f -o ..\artifact\44100\%%~nf_44100_sphere.mhr
+  ..\makemhr -r 48000 -j 2 -d dataset -i %%f -o ..\artifact\48000\%%~nf_48000_dataset.mhr
+  ..\makemhr -r 48000 -j 2 -d sphere -i %%f -o ..\artifact\48000\%%~nf_48000_sphere.mhr
+)
+
 cd ..\
