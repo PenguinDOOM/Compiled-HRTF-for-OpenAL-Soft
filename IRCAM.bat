7968eea3937adda2cca561b2cefa07cf074b594b
Penguin <hibi414sa@gmail.com>
2021-03-22 20:23:01 +0900

    Fixed a few things.

--- IRCAM.bat
+++ IRCAM.bat
@@ -1,13 +1,10 @@
-mkdir artifact\IRCAM\44100
-mkdir artifact\IRCAM\48000
-
 cd IRCAM
 
 for /R .\ %%f in (IRC_*.def) do (
-  ..\makemhr -r 44100 -j 2 -d dataset -i %%f -o ..\artifact\IRCAM\44100\%%~nf_44100_dataset.mhr
-  ..\makemhr -r 44100 -j 2 -d sphere -i %%f -o ..\artifact\IRCAM\44100\%%~nf_44100_sphere.mhr
-  ..\makemhr -r 48000 -j 2 -d dataset -i %%f -o ..\artifact\IRCAM\48000\%%~nf_48000_dataset.mhr
-  ..\makemhr -r 48000 -j 2 -d sphere -i %%f -o ..\artifact\IRCAM\48000\%%~nf_48000_sphere.mhr
+  ..\makemhr -r 44100 -j 2 -d dataset -i %%f -o ..\artifact\44100\%%~nf_44100_dataset.mhr
+  ..\makemhr -r 44100 -j 2 -d sphere -i %%f -o ..\artifact\44100\%%~nf_44100_sphere.mhr
+  ..\makemhr -r 48000 -j 2 -d dataset -i %%f -o ..\artifact\48000\%%~nf_48000_dataset.mhr
+  ..\makemhr -r 48000 -j 2 -d sphere -i %%f -o ..\artifact\48000\%%~nf_48000_sphere.mhr
 )
 
 cd ..\
