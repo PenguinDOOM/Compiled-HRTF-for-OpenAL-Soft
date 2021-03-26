2a0cd3fcbe4ad53542a7cbb698076c744be6da74
PenguinDOOM <hibi414sa@gmail.com>
2021-03-26 21:34:43 +0900

    Binary changed to 64bit.

--- SCUT.bat
+++ SCUT.bat
@@ -5,4 +5,9 @@ cd SCUT
 ..\makemhr -r 48000 -j 2 -d dataset -i SCUT_KEMAR.def -o ..\artifact\48000\scut_kemar_mono_48000_dataset.mhr
 ..\makemhr -r 48000 -j 2 -d sphere -i SCUT_KEMAR.def -o ..\artifact\48000\scut_kemar_mono_48000_sphere.mhr
 
+..\makemhr -r 44100 -j 2 -d dataset -i SCUT_KEMAR_stereo_test.def -o ..\artifact\44100\scut_kemar_stereo_44100_dataset.mhr
+..\makemhr -r 44100 -j 2 -d sphere -i SCUT_KEMAR_stereo_test.def -o ..\artifact\44100\scut_kemar_stereo_44100_sphere.mhr
+..\makemhr -r 48000 -j 2 -d dataset -i SCUT_KEMAR_stereo_test.def -o ..\artifact\48000\scut_kemar_stereo_48000_dataset.mhr
+..\makemhr -r 48000 -j 2 -d sphere -i SCUT_KEMAR_stereo_test.def -o ..\artifact\48000\scut_kemar_stereo_48000_sphere.mhr
+
 cd ..\
